5ce557c17254c816c1581a911b3653fc070c3c4e
Aditya <aditya.pratama@akarinti.tech>
2025-05-24 11:36:11 +0700

    checkpoint

--- tools/schedule_tool.py
+++ tools/schedule_tool.py
@@ -1,13 +1,21 @@
-import dateparser
+from pydantic import BaseModel,Field
 from langchain_core.tools import tool
+import dateparser
+import re
 
-
+class ScheduleInput(BaseModel):
+    event: str = Field(..., description="This is event name.")
+    time_info: str = Field(..., description="This is time info, format should 'YYYY MM DD HH:MM' ")
+   
 @tool
-def create_schedule(event: str, time_info: str) -> str:
+def create_schedule(input: ScheduleInput) -> str:
     """
-    Add schedule base on natural response.
-    Example time_info: 'malam Jumat jam 8'
+    Tambah jadwal berdasarkan input alami pengguna.
+    parsing kedalam format waktu yg valid
+    Contoh: 'acara rapat', 'malam Jumat jam 8'
     """
-    dt = dateparser.parse(time_info, languages=["id", "en"])
+    dt = dateparser.parse(input.time_info, languages=["id", "en"])
     if not dt:
         return "❌ Gagal memahami waktu."
+    return f"Jadwal '{input.event}' berhasil dibuat pada {dt.strftime('%A, %d %B %Y pukul %H:%M')}."
+
